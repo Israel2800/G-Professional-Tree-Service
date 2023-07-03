2129843d034684f06c85638f312ac51bd213ed8f
Dary <dady281100@gmail.com>
2023-07-03 00:11:47 -0500

    Navbar fixed

--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -9,11 +9,11 @@ export default function Navbar({ handlePageChange }) {
   };
 
   return (
-    <nav className="md:sticky top-0 z-10 container mx-auto flex flex-wrap p-5 pt-12 md:flex-row items-center justify-between">
+    <nav className="md:sticky top-0 z-10 bg-[#f3f8fe] md:flex p-5 pt-8 md:flex-row items-center justify-around">
       {/* Logo */}
-      <div className="flex items-center justify-center md:justify-start md:w-1/2">
+      <div className="flex items-center justify-center md:justify-start md:px-12 md:w-1/2">
         <img
-          className="object-cover object-center rounded-lg mb-8 leading-relaxed text-justify"
+          className="object-cover object-center rounded-lg leading-relaxed text-justify"
           alt="hero"
           src={GLogo}
           style={{ width: "100px", height: "100px" }}
@@ -21,7 +21,7 @@ export default function Navbar({ handlePageChange }) {
       </div>
 
       {/* Navigation Links */}
-      <div className="md:w-1/2 md:flex md:items-center md:justify-end">
+      <div className="container mx-auto flex flex-wrap p-5 pt-12 m- md:flex-row items-center justify-center md:text-center">
         {/* Menu Icon */}
         <button
           className="md:hidden text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
@@ -32,7 +32,7 @@ export default function Navbar({ handlePageChange }) {
 
         {/* Dropdown Menu */}
         {isMenuOpen && (
-          <div className="md:hidden absolute  right-0 bg-white w-full py-4 px-2">
+          <div className="md:hidden absolute top-0 right-0 bg-white w-full h-full py-4 px-2">
             <a
               href="#About"
               className="block text-gray-800 hover:text-gray-500 px-3 py-2 rounded-md text-sm font-medium"
@@ -86,52 +86,52 @@ export default function Navbar({ handlePageChange }) {
         )}
 
         {/* Normal Navigation */}
-        <div className="hidden md:flex md:items-center md:space-x-4">
+        <div className="hidden md:flex md:items-center justify-center text-black">
           <a
             href="#About"
-            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            className="hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
             onClick={() => handlePageChange('About')}
           >
             Home
           </a>
           <a
             href="#TreeRemoval"
-            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            className=" hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
             onClick={() => handlePageChange('TreeRemoval')}
           >
             Tree Removal
           </a>
           <a
             href="#StumpRemoval"
-            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            className=" hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
             onClick={() => handlePageChange('StumpRemoval')}
           >
             Stump Removal
           </a>
           <a
             href="#TreeTrimming"
-            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            className=" hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
             onClick={() => handlePageChange('TreeTrimming')}
           >
             Tree Trimming
           </a>
           <a
             href="#TreeCare"
-            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            className=" hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
             onClick={() => handlePageChange('TreeCare')}
           >
             Tree Care
           </a>
           <a
             href="#EmergencyCare"
-            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            className=" hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
             onClick={() => handlePageChange('EmergencyCare')}
           >
             Emergency Care
           </a>
           <a
             href="#Contact"
-            className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+            className=" hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
             onClick={() => handlePageChange('Contact')}
           >
             Contact
