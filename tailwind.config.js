4bf3241f5f29cc2b8aa853b5ba8d20677f7f80c3
Israel Aguilar <dady281100@gmail.com>
2023-08-01 21:44:19 -0600

    cleaned tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,11 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        // "hero": "url('https://images.pexels.com/photos/1632790/pexels-photo-1632790.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')",
         "mainImage": "url('../src/assets/gallery/port.png')",
         "treeRemoval": "url('../src/assets/images/tree-removal.jpg')",
         "treeTrimming": "url('../src/assets/images/tree-trimming.jpg')",
         "stumpRemoval": "url('../src/assets/images/stump-removal.jpg')",
         "treeCare": "url('../src/assets/images/tree-care.jpg')"
-        // "treeR": "url('../src/assets/images/3.jpg')"
       }
     },
   },
