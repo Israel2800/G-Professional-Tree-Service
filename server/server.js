dc9c23d4e2d12b06ddd5620ffef655afe7c8db8c
Dary <dady281100@gmail.com>
2023-08-03 22:56:38 -0500

    Adding server side for emails

---
+++ server/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+require("dotenv").config();
+
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Nodemailer setup
+const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE_PROVIDER,
+    auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+});
+  
+
+// API endpoint to handle sending confirmation emails
+app.post("/send-confirmation-email", (req, res) => {
+  const { to, subject, html } = req.body;
+
+  const mailOptions = {
+    from: "YOUR_EMAIL_ADDRESS",
+    to,
+    subject,
+    html,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+      res.status(500).json({ error: "Error sending email" });
+    } else {
+      console.log("Confirmation email sent:", info.response);
+      res.json({ success: "Confirmation email sent" });
+    }
+  });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
